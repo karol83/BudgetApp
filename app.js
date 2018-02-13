4a75b410469919d5baa1a01a3cc3baa64ef619a2
Karol on Windows <karol.drazynski@gmail.com>
2018-02-13 15:33:24 +0100

    Lecture 89

--- app.js
+++ app.js
@@ -169,6 +169,7 @@ var UIController = (function() {
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
         expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month',
         
     };
     
@@ -289,6 +290,17 @@ var UIController = (function() {
             
         },
         
+        displayMonth: function() {
+            var now, year, month, months;
+            
+            now = new Date();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            
+            month = now.getMonth();
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+        
        
         
         getDOMstrings: function() {
@@ -393,6 +405,7 @@ var controller = (function(budgetCtrl, UICtrl ) {
     return {
         init: function() {
             console.log('Application has started!');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
